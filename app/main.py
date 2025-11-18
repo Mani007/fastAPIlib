55e526fb056c6f2f9c6021782efb72da27ec5103
Mohnish <mohnish.a007@gmail.com>
2025-11-18 00:09:08 +0000

    first api with CORS

--- app/main.py
+++ app/main.py
@@ -1,10 +1,21 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
