e86d3abdeff10e55d5d99d1552a7e6a3938b6e1f
Mohnish <mohnish.a007@gmail.com>
2025-11-24 16:02:25 +0000

    asyncio python basic

--- basic python/async.py
+++ basic python/async.py
@@ -1,18 +1,36 @@
 # Async Await in python 
+# import time
+
+# def count():
+#     print("One")
+#     time.sleep(1)
+#     print("Two")
+#     time.sleep(1)
+
+# def main():
+#     for _ in range(3):
+#         count()
+
+# if __name__ == "__main__":
+#     start = time.perf_counter()
+#     main()
+#     elapsed = time.perf_counter() - start
+#     print(f"{__file__} executed in {elapsed:0.2f} seconds.")
+import asyncio
 import time
-
-def count():
+async def count():
     print("One")
-    time.sleep(1)
+    await asyncio.sleep(1)
     print("Two")
-    time.sleep(1)
+    await asyncio.sleep(1)
 
-def main():
-    for _ in range(3):
-        count()
+async def main():
+    await asyncio.gather(count(), count(), count())
 
 if __name__ == "__main__":
+    import time
+
     start = time.perf_counter()
-    main()
+    asyncio.run(main())
     elapsed = time.perf_counter() - start
     print(f"{__file__} executed in {elapsed:0.2f} seconds.")
