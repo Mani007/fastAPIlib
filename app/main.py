a44b992104a63eb04c02355ef726c3ceeee96e69
Mohnish <mohnish.a007@gmail.com>
2026-01-09 23:57:41 +0000

    Fastapi setup error

--- app/main.py
+++ app/main.py
@@ -1,13 +1,11 @@
 from typing import Union
-
+import uvicorn
+from app import app
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
-app = FastAPI()
-
 origins = [
     "http://localhost",
-    "http://localhost:8000",
+    "http://localhost:2500",
 ]
 app.add_middleware(
     CORSMiddleware,
@@ -16,11 +14,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id}
+if __name__ == '__main__':
+    uvicorn.run("main:app",host="127.0.0.1",port=2500,reload=True)
