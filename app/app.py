4becd2698c1d9d8e9a0803360fe880ca6d24d636
Mohnish <mohnish.a007@gmail.com>
2026-01-11 19:08:32 +0000

    Fastapi with dotenv setup working

--- app/app.py
+++ app/app.py
@@ -4,12 +4,12 @@ from fastapi import FastAPI
 
 load_dotenv()
 
-GCP = os.getenv('TEMPORARY')
+temp_key = os.getenv('TEMPORARY')
 
 app = FastAPI()
 
 
 @app.get("/")
 async def read_root():
-    print(GCP)
+    
     return {"Hello": "World"}
