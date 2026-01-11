b373d229b0df2b87506b3600b4dfc24351dad013
Mohnish <mohnish.a007@gmail.com>
2026-01-11 19:07:29 +0000

    Fastapi with dor env module working fine

--- app/app.py
+++ app/app.py
@@ -1,8 +1,15 @@
+import os
+from dotenv import load_dotenv
 from fastapi import FastAPI
+
+load_dotenv()
+
+GCP = os.getenv('TEMPORARY')
 
 app = FastAPI()
 
 
 @app.get("/")
 async def read_root():
+    print(GCP)
     return {"Hello": "World"}
