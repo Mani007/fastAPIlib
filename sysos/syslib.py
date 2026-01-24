2b902800d01a9dfc044504cc5b0e6ab4d8e38862
Mohnish <mohnish.a007@gmail.com>
2026-01-24 16:45:43 +0000

    Command line argument to exit in python

--- sysos/syslib.py
+++ sysos/syslib.py
@@ -1,15 +1,6 @@
 import sys
-n = len(sys.argv)
-
-print("Total arguments passed:", n)
-print("Name of Python script:", sys.argv[0])
-print("Arguments passed:", end=" ")
-
-for i in range(1, n):
-    print(sys.argv[i], end=" ")
-
-Sum = 0
-for i in range(1, n):
-    Sum += int(sys.argv[i])
-
-print(Sum)
+age = input("Enter your age: ")
+if int(age) < 18:
+    sys.exit("Age less than 18")
+else:
+    print("Age is not less than 18")
